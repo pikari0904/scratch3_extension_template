a1553fe3383560c99cd0de047ee820405d0e5db8
pikari0904 <50161725+pikari0904@users.noreply.github.com>
2022-02-15 20:51:54 +0900

    ver0.1

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ LF=$(printf '\\\012_')
 LF=${LF%_}
 EXTENSION_NAME=Hello
 EXTENSION_ID=hello
-COLLABORATOR=champierre
+COLLABORATOR=pikari
 EXTENSION_DESCRIPTION="Scratch Extension Template"
 
 mkdir -p node_modules/scratch-vm/src/extensions/scratch3_${EXTENSION_ID}
